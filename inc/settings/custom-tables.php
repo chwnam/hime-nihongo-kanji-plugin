9ca810d1cb330e34b127e8cc6118e83475322987
changwoo <ep6tri@hotmail.com>
2026-03-13 03:05:27 +0900

    DB 버전 관련 정보에 상수 적용

--- inc/settings/custom-tables.php
+++ inc/settings/custom-tables.php
@@ -5,12 +5,12 @@ if (!defined('ABSPATH')) {
 }
 
 return [
-    'version_name'    => 'hnkp_db_version', // Optional
-    'version'         => '0.2.0',           // Optional
-    'is_theme'        => false,             // Optional, defaults to false.
-    'main_file'       => HNKP_MAIN,         // Optional, defaults to blank.
-    'activation'      => true,              // Optional, defaults to false. Create tables on activation.
-    'deactivation'    => false,             // Optional, defaults to false. [Delete] tables on deactivation.
-    'uninstall'       => false,             // Optional, defaults to false. [Delete] tables on uninstall.
-    'suppress_errors' => false,             // Optional, defaults to false.
+    'version_name'    => HNKP_DB_VERSION_NAME, // Optional
+    'version'         => HNKP_DB_VERSION,      // Optional
+    'is_theme'        => false,                // Optional, defaults to false.
+    'main_file'       => HNKP_MAIN,            // Optional, defaults to blank.
+    'activation'      => true,                 // Optional, defaults to false. Create tables on activation.
+    'deactivation'    => false,                // Optional, defaults to false. [Delete] tables on deactivation.
+    'uninstall'       => false,                // Optional, defaults to false. [Delete] tables on uninstall.
+    'suppress_errors' => false,                // Optional, defaults to false.
 ];
