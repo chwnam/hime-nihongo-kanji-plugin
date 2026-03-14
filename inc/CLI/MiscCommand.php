f6a6810dc15ca9f686c918bfb8d02d96e607f9f1
changwoo <ep6tri@hotmail.com>
2026-03-15 01:22:25 +0900

    카스미 CSV 입력 스크립트 추가

--- inc/CLI/MiscCommand.php
+++ inc/CLI/MiscCommand.php
@@ -4,6 +4,7 @@ namespace HimeNihongo\KanjiPlugin\CLI;
 
 use Bojaghi\CustomTables\CustomTables;
 use WP_CLI;
+use WP_CLI\ExitException;
 
 /**
  * Table support commands.
@@ -59,5 +60,95 @@ class MiscCommand
         $ct->deleteTables();
 
         WP_CLI::success('Tables dropped.');
+    }
+
+    /**
+     * 카스미 버전 CSV 데이터를 테이블로 삽입합니다.
+     *
+     * ## EXAMPLES
+     *
+     *     wp hnkp/misc insert-csv n5.csv n4.csv n3.csv
+     *
+     * ## OPTIONS
+     *
+     * <csv_path>...
+     * : CSV 파일 경로를 입력합니다. 여러 개의 파일을 입력할 수 있습니다.
+     *
+     * @subcommand insert-csv
+     * @when       after_wp_load
+     *
+     * @param array $args
+     *
+     * @return void
+     * @throws ExitException
+     */
+    public function insertCsv(array $args): void
+    {
+        global $wpdb;
+
+        $tableName = "{$wpdb->prefix}hnkp_kasumi_words";
+
+        foreach ($args as $path) {
+            if (!file_exists($path) || !is_readable($path)) {
+                WP_CLI::error("File not found, or unreadable: $path");
+            }
+        }
+
+        $wpdb->query("TRUNCATE TABLE `$tableName`");
+        $wpdb->query("START TRANSACTION");
+
+        foreach ($args as $path) {
+            // Read JLPT level from file name.
+            $name = pathinfo($path, PATHINFO_FILENAME);
+            if (!preg_match('/^n(\d+)$/', $name, $matches)) {
+                WP_CLI::error("Invalid file name: $name");
+            }
+            $jlpt = (int)$matches[1];
+
+            // Read CSV first.
+            $rows = [];
+            if (!($fp = fopen($path, 'r'))) {
+                WP_CLI::error("Failed to open file: $path");
+            }
+            while (false !== ($row = fgetcsv($fp, 1000, ',', '"', '\\'))) {
+                $rows[] = $row;
+            }
+            fclose($fp);
+
+            // Insert entries from rows
+            $entry = -1;
+
+            foreach ($rows as $row) {
+                $row  = array_filter(array_map('trim', $row));
+                $col1 = (int)$row[0];
+
+                if ($col1 > $entry) {
+                    // 한자 정의 - 스킵
+                    $entry = $col1;
+                    continue;
+                }
+
+                if ($entry === $col1) {
+                    // 단어 의미
+                    $query = $wpdb->prepare(
+                        "INSERT IGNORE INTO `$tableName` (jlpt, entry, word, yomikata, meaning) " .
+                        "VALUES (%d, %d, %s, %s, %s)",
+                        $jlpt,
+                        $entry,
+                        $row[1],
+                        $row[2],
+                        $row[3],
+                    );
+                    WP_CLI::log($query);
+                    $wpdb->query($query);
+                    if ($wpdb->last_error) {
+                        WP_CLI::error($wpdb->last_error);
+                    }
+                }
+            }
+        }
+
+        $wpdb->query("COMMIT");
+        WP_CLI::success("Successfully imported.");
     }
 }
