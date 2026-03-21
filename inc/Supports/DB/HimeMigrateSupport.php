e03837199a6b70d4b34eab81bee1de0611c95069
changwoo <ep6tri@hotmail.com>
2026-03-22 03:04:21 +0900

    오타 수정
    --
    그래도 쿼리에 한계가 있음
    매핑 테이블과 전체적으로 한자의 노멀라이즈가 들어가야 함 그렇지 않으면 제대로 값이 들어가지 않음

--- inc/Supports/DB/HimeMigrateSupport.php
+++ inc/Supports/DB/HimeMigrateSupport.php
@@ -53,7 +53,7 @@ class HimeMigrateSupport implements Support
             "    MAX(h.level) AS ko_level\n" .
             "FROM `$tableKanji` k\n" .
             "LEFT JOIN `$tableJlpt` j ON j.kanji = k.kanji\n" .
-            "LEFT JOIN `$tableMap` m1 ON m1.k_in = k.kanji AND m1.type IN ('tv', 'sev', 'z')\n" .
+            "LEFT JOIN `$tableMap` m1 ON m1.k_in = k.kanji AND m1.type IN ('tv', 'sev', 'zv')\n" .
             "LEFT JOIN `$tableMap` m2 ON m2.k_out = k.kanji AND m2.type = 'siv'\n" .
             "LEFT JOIN `$tableSinji` s1 ON s1.kanji = m1.k_out\n" .
             "LEFT JOIN `$tableSinji` s2 ON s2.kanji = m2.k_out\n" .
